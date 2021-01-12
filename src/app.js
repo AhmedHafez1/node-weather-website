7ac261871aeaafaefdb025747cf68565757cce93
unknown <ahmed.abdelghany.hafez@gmail.com>
2021-01-12 18:35:21 +0200

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast');
 
 const app = express();
 
+const port = process.env.PORT || 3000
+
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -89,7 +91,7 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port' + port);
 });
 
